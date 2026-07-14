e04248074ef1eeb5a54e65dd468332d7689b6fd3
bspafford <bspafford05@gmail.com>
2026-07-13 23:58:14 -0400

    Got a very basic search system to work

--- backend/server/main.cpp
+++ backend/server/main.cpp
@@ -4,27 +4,29 @@
 #include <sstream>
 #include "../login.h"
 
-std::string ExecuteSQL();
+std::string ExecuteSQL(const std::string& query);
 
 pqxx::connection cx("host=localhost dbname=SearchEngine user=" + USER + " password=" + PASSWORD);
 
 int main() {
-    uWS::App().post("/api/search", [](auto *res, auto *req) {
-        // read the POST body
-        res->onData([](std::string_view data, bool last) {
-            if (last) {
-                    std::cout << "Received: " << data << "\n";
-            }
-        });
+    int port = 8080;
 
-        std::string str = ExecuteSQL();
+    uWS::App().get("/search", [](uWS::HttpResponse<false> *res, uWS::HttpRequest *req) {
+
+        std::string_view query = req->getQuery("q");
+        std::cout << req->getUrl() << ", query: " << query << "\n";
+
+        std::string output = ExecuteSQL(std::string(query));
         
-        res->writeHeader("Content-Type", "application/text");
-        res->end(str);
+        // std::stringstream ss;
+        // ss << "<html><body><h1>Hello world!</h1><p>you search for \"" << query << "\"</p></body></html>";
+
+        res->writeHeader("Content-Type", "text/html");
+        res->end(output);
     })
-    .listen(8080, [](auto *listenSocket) {
+    .listen(port, [port](auto *listenSocket) {
         if (listenSocket) {
-            std::cout << "Listening on 8080\n";
+            std::cout << "Listening on " << port << "\n";
         }
     })
     .run();
@@ -32,16 +34,19 @@ int main() {
 } 
 
 
-std::string ExecuteSQL() {
+std::string ExecuteSQL(const std::string& query) {
     // start a transaction
     pqxx::work tx{cx};
 
     std::stringstream output;
 
-    for (auto [id, url, title, description, contentHash, lastVisited] : tx.stream<long long, std::string_view, std::string_view, std::string_view, long long, std::string_view> (
-        "SELECT * FROM siteData")) {
+    std::string pattern = "%" + query + "%";
+    // for (auto [id, url, title, description, contentHash, lastVisited] : tx.stream<long long, std::string_view, std::string_view, std::string_view, long long, std::string_view> (
+        // "SELECT * FROM siteData WHERE title LIKE $1", pattern)) {
+    auto result = tx.exec("SELECT * FROM siteData WHERE title ILIKE $1", pqxx::params(pattern));
 
-         output << "id: " << id << "\nurl: " << url << "\ntitle: " << title << "\ndescription: " << description << "\ncontentHash: " << contentHash << "\nlastVisited: " << lastVisited << "\n\n";
+    for (auto row : result) {
+         output << "id: " << row["id"].c_str() << "\nurl: " << row["url"].c_str() << "\ntitle: " << row["title"].c_str() << "\ndescription: " << row["description"].c_str() << "\ncontentHash: " << row["contentHash"].c_str() << "\nlastVisited: " << row["lastVisited"].c_str() << "\n\n";
     }
 
     // Commit the transaction
